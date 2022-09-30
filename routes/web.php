cb231059d4bc64ab6309e24fe95f97632bc1d7df
unkxwnXgxd <jgbuguhuhuhuhhhuh@gmail.com>
2022-09-30 21:24:15 +0700

    finished

--- routes/web.php
+++ routes/web.php
@@ -23,15 +23,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/abiturients/edit/{abiturient}', [App\Http\Controllers\AbiturientController::class, 'edit'])->name('abi_edit');
 Route::put('/edit/{abiturient}', [App\Http\Controllers\AbiturientController::class, 'update'])->name('abi_update');
 Route::delete('/edit/{abiturient}', [App\Http\Controllers\AbiturientController::class, 'destroy'])->name('abi_delete');
-
 Route::get('/create', [App\Http\Controllers\AbiturientController::class, 'create'])->name('abi_create');
 Route::put('/create', [App\Http\Controllers\AbiturientController::class, 'store'])->name('abi_create');
-
-Route::get('/abi', function () {
-    return view('abifront');
-});
 Route::get('/spec', [App\Http\Controllers\SpecialityController::class, 'index'])->name('spec_index');
 Route::get('/', [App\Http\Controllers\AbiturientController::class, 'index'])->name('main_index');
 Route::get('/abiturients/{abiturient}', [App\Http\Controllers\AbiturientController::class, 'show'])->name('abi_index');
-
 Route::get('/doc', [App\Http\Controllers\DocumenttypeController::class, 'index'])->name('doc_index');
